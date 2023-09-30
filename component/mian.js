f6e87917f0abadf1912fe0e5805ed249a30eb19f
deepshikhalcb049 <lcb2021049@iiitl.ac.in>
2023-10-01 00:49:54 +0530

    new

--- component/mian.js
+++ component/mian.js
@@ -216,12 +216,12 @@ lati=e.target.value
             
 
             
-
-            <button onClick={fetchapi} className="w-[90px] p-2 mt-[20px] border-solid border-gray-600 border-[2px] rounded-[6px] hover:bg-black hover:text-white ">call api</button>
             {
                 (loading) ? "Loading" : ""
 
             }
+            <button onClick={fetchapi} className="w-[90px] p-2 mt-[20px] border-solid border-gray-600 border-[2px] rounded-[6px] hover:bg-black hover:text-white ">call api</button>
+            
 
 
             {(done) ? (
